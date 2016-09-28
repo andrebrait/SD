006bd23c2ad055163f539fb2cf487c0e8d1275a9
Andre Brait <andrebrait@gmail.com>
2016-09-27 22:50:46 -0300

    Pequenas mudanças

--- TP1/RPC/fileserv.c
+++ TP1/RPC/fileserv.c
@@ -7,7 +7,9 @@ char **servefile_1_svc(char **fname, struct svc_req *req) {
     static char *buffer;
 
     fp = fopen(*fname, "r");
-    if (!fp) perror(*fname), exit(1);
+    if (!fp) {
+        perror(*fname), exit(1);
+    }
 
     fseek(fp, 0L, SEEK_END);
     lSize = ftell(fp);
@@ -16,11 +18,14 @@ char **servefile_1_svc(char **fname, struct svc_req *req) {
     /* allocate memory for entire content */
     buffer = calloc(1, (size_t) lSize + 1);
 
-    if (!buffer) fclose(fp), fputs("memory alloc fails", stderr), exit(1);
+    if (!buffer) {
+        fclose(fp), fputs("memory alloc fails", stderr), exit(1);
+    }
 
     /* copy the file into the buffer */
-    if (1 != fread(buffer, (size_t) lSize, 1, fp))
+    if (1 != fread(buffer, (size_t) lSize, 1, fp)) {
         fclose(fp), free(buffer), fputs("entire read fails", stderr), exit(1);
+    }
 
     /* do your work here, buffer is a string contains the whole text */
     fclose(fp);
